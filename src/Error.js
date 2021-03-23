9c758e3ad133169fd79c3627d00287f712658b60
Peter Staker <peterjast@gmail.com>
2021-03-22 18:01:34 -0700

    adding a search bar and rendering location data

---
+++ src/Error.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ErrorMessage extends React.Component {
+  
+    render() {
+        return(
+            <>
+            <h3>Something went wrong. Please Try again!</h3>
+            </>
+        )}
+  }
+
+export default ErrorMessage;
